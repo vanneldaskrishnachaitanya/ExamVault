77ca6f5faf198e5b52d62cbb5491c9b11f059dd4
Krishna Chaitanya <vanneldaskrishnachaitanya.com>
2026-04-03 20:11:48 +0530

    Update app loader text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,7 +58,7 @@ function AppLoader() {
   return (
     <div className="app-loader">
       <div className="app-loader__ring" />
-      <span className="app-loader__text">VNRVJIET</span>
+      <span className="app-loader__text">Tamso Maa Jyotirgamay</span>
     </div>
   );
 }
